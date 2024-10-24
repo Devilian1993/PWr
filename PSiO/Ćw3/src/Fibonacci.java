20fa4ab45133abec24a562855cf8d3350ce858f2
LENOVO <maksioczko@gmail.com>
2024-10-24 13:16:40 +0200

    Lab3

--- PSiO/Ćw3/src/Fibonacci.java
+++ PSiO/Ćw3/src/Fibonacci.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Fibonacci {
 
     public static void n_fibonacci(int n) {
